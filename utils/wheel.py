a2f0cc41705bef5d8f60c0ddd7129a0ee1c7ac81
StrikerCC <chengc0611@gmail.com>
2021-04-24 01:07:32 +0800

    a

--- utils/wheel.py
+++ utils/wheel.py
@@ -208,7 +208,7 @@ class wheelControlled(wheel):
         time.sleep(0.01)
 
         for _ in range(100):
-            print(360.0 - angle_goal - self._tolerance, 'in', self.imu_.angle(), 'in',
+            print(360.0 - angle_goal - self._tolerance, '<', self.imu_.angle(), '<',
                   360.0 - angle_goal + self._tolerance)
             if 360.0 - angle_goal - self._tolerance <= self.imu_.angle() <= 360.0 - angle_goal + self._tolerance:
                 print(angle_init, 'to', angle_goal)
@@ -235,7 +235,7 @@ class wheelControlled(wheel):
         time.sleep(0.01)
 
         for _ in range(100):
-            print(angle_goal - self._tolerance, '<', self.imu_.angle() < angle_goal, '<', self._tolerance)
+            print(angle_goal - self._tolerance, '<', self.imu_.angle(), '<', angle_goal + self._tolerance)
             if angle_goal - self._tolerance < self.imu_.angle() < angle_goal + self._tolerance:
                 print(angle_init, 'to', angle_goal)
                 print('reach', self.imu_.angle())
