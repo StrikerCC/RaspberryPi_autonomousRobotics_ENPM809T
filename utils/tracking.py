f20540c00ff82c3347b63451b53cdd659d88d65a
StrikerCC <chengc0611@gmail.com>
2021-05-04 01:33:07 +0800

    drive

--- utils/tracking.py
+++ utils/tracking.py
@@ -56,19 +56,48 @@ qrcode_results = ['J&J', 'MODERNA', 'PFIZER']
 #
 #     return None
 
-def get_qrcode():
-    command = 'sudo modprobe bcm2835-v4l2'
-    os.system(command)
+# def get_qrcode():
+#     command = 'sudo modprobe bcm2835-v4l2'
+#     os.system(command)
+#
+#     # open video capture
+#     cap = cv2.VideoCapture(0)
+#
+#     # define detector
+#     detector = cv2.QRCodeDetector()
+#     data = None
+#
+#     while True:
+#         check, img = cap.read()
+#         img = cv2.flip(img, -1)
+#         data, bbox, _ = detector.detectAndDecode(img)
+#
+#         if bbox is not None:
+#             for i in range(len(bbox)):
+#                 cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i + 1) % len(bbox)][0]), color=(0, 0, 255), thickness=4)
+#
+#         if data is not None:
+#             print("Data: ", data)
+#             if data in qrcode_results:
+#                 break
+#
+#         # show frame
+#         cv2.imshow("QR code detector", img)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     return data
 
-    # open video capture
-    cap = cv2.VideoCapture(0)
 
+def get_qrcode(camera_):
     # define detector
     detector = cv2.QRCodeDetector()
     data = None
 
     while True:
-        check, img = cap.read()
+        img = camera_.view_one_frame()
         img = cv2.flip(img, -1)
         data, bbox, _ = detector.detectAndDecode(img)
 
@@ -86,7 +115,6 @@ def get_qrcode():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
     cv2.destroyAllWindows()
     return data
 
