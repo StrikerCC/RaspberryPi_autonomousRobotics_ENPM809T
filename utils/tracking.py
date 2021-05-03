e928b6f86c4dba62dda0d2deeabc979bdd98049b
StrikerCC <chengc0611@gmail.com>
2021-05-04 01:05:10 +0800

    drive

--- utils/tracking.py
+++ utils/tracking.py
@@ -16,45 +16,46 @@ sys.path.insert(0, os.path.dirname(os.getcwd()))
 import numpy as np
 import cv2
 
+import cv2
 from imutils.video import VideoStream
 import imutils
 import time
 
-from RaspberryPi_autonomousRobotics_ENPM809T.utils.image import find_ROI
+# from RaspberryPi_autonomousRobotics_ENPM809T.utils.image import find_ROI
 
 qrcode_results = ['J&J', 'MODERNA', 'PFIZER']
 
-def angle_of_object(camera_, color_limit_object):
-    frames_out = 10000
-
-    print('looking for object like ', object, 'for', frames_out, 'frames')
-
-    """find object in current frame"""
-    for i in range(frames_out):
-        img = camera_.view_one_frame()
-
-        """find a contour around the object"""
-        center, area = find_ROI(img, color_limit_object)
-        radius = np.sqrt(area/2/np.pi)
-
-        cv2.circle(img, center, int(radius), (255, 155, 155), 1)
-        cv2.imshow(str(center), img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        """calculate the pixel coord"""
-        angle = camera_.coord_img_to_pose(center)
-        angle[0] = -angle[0]
-
-        if area > 5.0:    # if the pixel cluster is big enough
-            """transform to img coord"""
-            print('frame', i, 'found object at', angle, 'degree')
-            return angle
-
-        if i % 100 == 0:
-            print('frame', i, 'haven\'t find object yet')
-
-    return None
+# def angle_of_object(camera_, color_limit_object):
+#     frames_out = 10000
+#
+#     print('looking for object like ', object, 'for', frames_out, 'frames')
+#
+#     """find object in current frame"""
+#     for i in range(frames_out):
+#         img = camera_.view_one_frame()
+#
+#         """find a contour around the object"""
+#         center, area = find_ROI(img, color_limit_object)
+#         radius = np.sqrt(area/2/np.pi)
+#
+#         cv2.circle(img, center, int(radius), (255, 155, 155), 1)
+#         cv2.imshow(str(center), img)
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
+#
+#         """calculate the pixel coord"""
+#         angle = camera_.coord_img_to_pose(center)
+#         angle[0] = -angle[0]
+#
+#         if area > 5.0:    # if the pixel cluster is big enough
+#             """transform to img coord"""
+#             print('frame', i, 'found object at', angle, 'degree')
+#             return angle
+#
+#         if i % 100 == 0:
+#             print('frame', i, 'haven\'t find object yet')
+#
+#     return None
 
 def get_qrcode():
     command = 'sudo modprobe bcm2835-v4l2'
@@ -93,8 +94,8 @@ def get_qrcode():
 
 def face_detect():
     # construct the argument parse and parse the arguments
-    prototxt = '/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/deploy.prototxt.txt',
-    model = '/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/res10_300x300_ssd_iter_140000.caffemodel',
+    prototxt = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/deploy.prototxt.txt',
+    model = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/res10_300x300_ssd_iter_140000.caffemodel',
     confidence_min = 0.5,
 
     # load our serialized model from disk
