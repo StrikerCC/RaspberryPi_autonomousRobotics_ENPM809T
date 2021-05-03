509bb076002fb7c56aa559c1c952fcb4615ecbfa
StrikerCC <chengc0611@gmail.com>
2021-05-04 01:09:43 +0800

    drive

--- utils/tracking.py
+++ utils/tracking.py
@@ -94,14 +94,17 @@ def get_qrcode():
 
 def face_detect():
     # construct the argument parse and parse the arguments
-    prototxt = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/deploy.prototxt.txt',
-    model = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/res10_300x300_ssd_iter_140000.caffemodel',
-    confidence_min = 0.5,
+    prototxt = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/deploy.prototxt.txt'
+    model = r'/home/pi/ENPM809T/RaspberryPi_autonomousRobotics_ENPM809T/utils/res10_300x300_ssd_iter_140000.caffemodel'
+    confidence_min = 0.5
 
     # load our serialized model from disk
     print("[INFO] loading model...")
-    net = cv2.dnn.readNetFromCaffe(prototxt, model)
-
+    net = None
+    try:
+        net = cv2.dnn.readNetFromCaffe(prototxt, model)
+    except:
+        print('cannot load model', model)
     print("[INFO] starting video stream...")
     vs = VideoStream(src=0).start()
     time.sleep(2.0)
@@ -118,42 +121,44 @@ def face_detect():
 
         # grab the frame dimensions and convert it to a blob
         (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-                                     (300, 300), (104.0, 177.0, 123.0))
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        net.setInput(blob)
-        detections = net.forward()
+        if net is not None:
+            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                         (300, 300), (104.0, 177.0, 123.0))
 
-        # loop over the detections
-        for i in range(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with the
-            # prediction
-            confidence = detections[0, 0, i, 2]
+            # pass the blob through the network and obtain the detections and
+            # predictions
+            net.setInput(blob)
+            detections = net.forward()
 
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence < confidence_min:
-                continue
+            # loop over the detections
+            for i in range(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with the
+                # prediction
+                confidence = detections[0, 0, i, 2]
 
-            # compute the (x, y)-coordinates of the bounding box for the
-            # object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+                # filter out weak detections by ensuring the `confidence` is
+                # greater than the minimum confidence
+                if confidence < confidence_min:
+                    continue
 
-            # draw the bounding box of the face along with the associated
-            # probability
-            text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          (0, 255, 0), 2)
-            cv2.putText(frame, text, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-            cv2.imshow("Frame", cv2.flip(frame, -1))
-            key = cv2.waitKey(5) & 0xFF
-            return frame
-            # break
+                # compute the (x, y)-coordinates of the bounding box for the
+                # object
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # draw the bounding box of the face along with the associated
+                # probability
+                text = "{:.2f}%".format(confidence * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              (0, 255, 0), 2)
+                cv2.putText(frame, text, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+                cv2.imshow("Frame", cv2.flip(frame, -1))
+                key = cv2.waitKey(5) & 0xFF
+                return frame
+                # break
 
         # show the output frame
         cv2.imshow("Frame", cv2.flip(frame, -1))
